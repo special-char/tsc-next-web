be9504eb707616517550f55477af152943f4c18d
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2022-11-11 12:41:22 +0530

    svg problem

--- app/training/faq.tsx
+++ app/training/faq.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '@/styles/faq.css';
+import PluseSvg from '../../public/icons/pluse.svg';
 
 interface Props {}
 const Data = [
@@ -10,25 +11,25 @@ const Data = [
       'Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor.',
   },
   {
-    id: '1',
+    id: '2',
     title: 'Do you offer discounts for students?',
     Description:
       'Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor.',
   },
   {
-    id: '1',
+    id: '3',
     title: 'Do you offer discounts for students?',
     Description:
       'Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor.',
   },
   {
-    id: '1',
+    id: '4',
     title: 'Do you offer discounts for students?',
     Description:
       'Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor.',
   },
   {
-    id: '1',
+    id: '5',
     title: 'Do you offer discounts for students?',
     Description:
       'Lorem ipsum dolor sit amet, consectetur dolorili adipiscing elit. Felis donec massa aliquam id dolor.',
@@ -39,23 +40,19 @@ const Faq = (props: Props) => {
   return (
     <section id="Faq" className="faq">
       <div className="faq__section ">
-        <div className="faq__page ">
+        <div className="faq__header">
           <h2>Frequently Asked Questions.</h2>
           <p>
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
             nisi ut aliquip ex ea commodo consequat.
           </p>
         </div>
-        <div className="faq__downPage">
+        <div className="faq__body">
           {Data.map((val) => (
-            <div key={val.id}>
-              <div className="faq__pageSection">
-                <details className="accordion">
-                  <summary className="accordion__summary">{val.title}</summary>
-                  <p className="accordion__content">{val.Description}</p>
-                </details>
-              </div>
-            </div>
+            <details className="accordion">
+              <summary className="accordion__summary">{val.title}</summary>
+              <p className="accordion__content">{val.Description}</p>
+            </details>
           ))}
         </div>
       </div>
