94c596706f8b694e5b7bf5dc591e77aa0d0fcde1
Ankita Trivedi <iamtrivediankita@gmail.com>
2022-11-16 17:07:44 +0530

    found

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -2,6 +2,9 @@ import Banner from './banner';
 import Category from './category';
 import Courses from './courses';
 import Education from './education';
+import Event from './event';
+import Features from './features';
+import Found from './found';
 import GrowCareer from './growcareer';
 import Leaders from './leaders';
 import Perks from './perks';
@@ -13,14 +16,16 @@ const rating = 4;
 const Page = (props: Props) => {
   return (
     <>
-      <Banner />
+      {/* <Banner />
+
       <Courses />
       <Perks />
       <Leaders />
       <GrowCareer />
       <Education />
-      <Category />
+      <Category /> */}
       {/* <Blogs /> */}
+      <Found />
     </>
   );
 };
