b3e0500248afb5f7bc4c669c6cf809210bb560dc
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-11-16 14:58:13 +0530

    completed rating component

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,35 +1,27 @@
-import Card from '@/ui/Card';
-import Aboutheader from './aboutheader';
-import Achievements from './achievements';
 import Banner from './banner';
-import Blogs from './blogs';
 import Category from './category';
-import Contact from './contact/contact';
 import Courses from './courses';
 import Education from './education';
-import Event from './event';
-import Features from './features';
 import GrowCareer from './growcareer';
 import Leaders from './leaders';
 import Perks from './perks';
-import SocialMedia from './socialMedia';
-import TeacherCard from './teachercard';
 
 type Props = {};
 
+const rating = 4;
+
 const Page = (props: Props) => {
   return (
-    <div className="bg-secondary1 p-14">
-      <Card />
-      {/* <Banner />
+    <>
+      <Banner />
       <Courses />
       <Perks />
       <Leaders />
       <GrowCareer />
       <Education />
       <Category />
-      <Blogs /> */}
-    </div>
+      {/* <Blogs /> */}
+    </>
   );
 };
 
