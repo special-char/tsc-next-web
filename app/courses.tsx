30c7e89203f51fe1107bd95239584880d245dfe1
Hemang <hemangmesariya009@gmail.com>
2022-12-09 13:04:59 +0530

    api integrated faq

--- app/courses.tsx
+++ app/courses.tsx
@@ -79,6 +79,7 @@ async function getCoursesData() {
                   data{
                     id
                     attributes{
+                      alternativeText
                       url
                     }
                   }
@@ -104,30 +105,34 @@ const Courses = (props: Props) => {
   if (!coursesData) return null;
 
   const coursesInfo = coursesData?.data?.courses?.data;
+
   return (
     <section id="courses" className="courses">
       <h2 className="courses__title ">Browse our popular courses</h2>
       <Carousal>
-        {coursesInfo.map((course) => (
-          <Link key={course.id} href="/" className="card">
-            <figure className="card__image">
-              <Image src={course.url} alt={course.attributes.title} fill />
-            </figure>
-            <div className="card__body">
-              <h3 className="card__title">{course.attributes.title}</h3>
-              <p className="card__desc">{course.attributes.description}</p>
-              <div className="card__actions">
-                <div className="avatar placeholder w-16 overflow-hidden rounded-full">
-                  <img
-                    src="https://assets.website-files.com/607de2d8e8911ebf197a3f0f/607f1ff9fd9e0e1686d26497_image-3-profile-picture-small-teacher-education-x-template.jpg"
-                    alt="Kathie Corl"
-                  />
+        {coursesInfo.map((course) => {
+          const { title, description } = course?.attributes;
+          return (
+            <Link key={course.id} href="/" className="card">
+              {/* <figure className="card__image">
+                <Image src={course.url} alt={course.name} fill />
+              </figure> */}
+              <div className="card__body">
+                {/* <h3 className="card__title">{title}</h3> */}
+                {/* <p className="card__desc">{description}</p> */}
+                <div className="card__actions">
+                  <div className="avatar placeholder w-16 overflow-hidden rounded-full">
+                    <img
+                      src="https://assets.website-files.com/607de2d8e8911ebf197a3f0f/607f1ff9fd9e0e1686d26497_image-3-profile-picture-small-teacher-education-x-template.jpg"
+                      alt="Kathie Corl"
+                    />
+                  </div>
+                  <h4>Yagnesh Modh</h4>
                 </div>
-                <h4>Yagnesh Modh</h4>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </Carousal>
 
       <Link href="/" className="btn btn--primary">
