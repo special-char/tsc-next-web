2f32a7bfb0b1f6a56be069c645d909c801424a01
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-12 17:09:47 +0530

    commit code

--- ui/Button.tsx
+++ ui/Button.tsx
@@ -1,9 +1,34 @@
+import clsx from 'clsx';
 import React from 'react';
 
-type Props = {};
+type Props = {
+  type: string;
+  size: string;
+  isRound: boolean;
+  className?: string;
+};
 
-const Button = (props: Props) => {
-  return <>Button</div>;
+const Button = ({
+  as: Tag,
+  type,
+  size,
+  isRound,
+  className,
+  ...rest
+}: Props) => {
+  return (
+    <Tag
+      className={clsx('btn', {
+        'btn--primary': type === 'primary',
+        'btn--secondary': type === 'secondary',
+        'btn--small': size === 'small',
+        'btn--large': size === 'large',
+        'btn--round': isRound,
+        [className || '']: !!className,
+      })}
+      {...rest}
+    />
+  );
 };
 
 export default Button;
--- ui/CustomForm.tsx
+++ ui/CustomForm.tsx
@@ -2,6 +2,7 @@ import { Formik, Form, Field } from 'formik';
 import Link from 'next/link';
 import React from 'react';
 import Button from './Button';
+import Icon from './Icon';
 
 type Props = {};
 
@@ -13,9 +14,15 @@ const CustomForm = ({ fields, ...rest }: Props) => {
           {fields.map((field) => {
             return <Field key={field.name} {...field} />;
           })}
-          <button type="submit" className="btn btn--primary">
+          <Button as={Link} href="/" type="secondary" isRound className="w-10">
+            <Icon name="facebook" height={16} width={16} fill="red" />
+          </Button>
+          <Button as={Link} href="/" type="secondary" size="large">
+            Hello
+          </Button>
+          {/* <button type="submit" className="btn btn--primary">
             submit
-          </button>
+          </button> */}
         </Form>
       )}
     </Formik>
