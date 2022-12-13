eb5a1fe2fe0425fcdfc4d600e20c028b45b57738
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-13 11:51:44 +0530

    commit code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import '@/styles/globals.css';
-import '@/styles/sideNav.css';
 import Link from 'next/link';
 import React from 'react';
 import Footer from './footer';
 import Header from './header';
+import SideNav from './sideNav';
 
 export default function RootLayout({
   children,
@@ -14,16 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <aside id="sidenav-open">
-          <nav></nav>
-          <a
-            href="#"
-            id="sidenav-close"
-            title="Close Menu"
-            aria-label="Close Menu"
-            // onchange="history.go(-1)"
-          ></a>
-        </aside>
+        <SideNav />
         <div>
           <Header />
           <main>{children}</main>
