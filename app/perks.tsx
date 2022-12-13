567b74747d181011742b5713a298146a06dce286
dhara dave <imdavedhara@gmail.com>
2022-12-13 16:26:14 +0530

    skeleton

--- app/perks.tsx
+++ app/perks.tsx
@@ -4,6 +4,38 @@ import Image from 'next/image';
 import { getPerksData } from '@/lib/getPerks';
 import { ComponentCommonDetail, Perk, UploadFile } from 'types/types';
 
+export const PerksSkeleton = () => {
+  return (
+    <section id="perks" className="perks ">
+      <h2 className="perks__title animate-pulse font-cursive">
+        Why learn with our courses?
+      </h2>
+      <div className="perks__content  animate-pulse">
+        {[1, 2, 3].map((perk) => {
+          //   const { title, description, image, id } =
+          //     perk as ComponentCommonDetail;
+          //   const { url } = image.data?.attributes as UploadFile;
+          return (
+            <div key={perk} className="perks__item">
+              <div className="perks__item__image ">
+                <div></div>
+              </div>
+              <div className="perks__item__body">
+                <h3 className="mb-4 font-cursive text-neutral-100">1.Learn</h3>
+                <p className="mb-0 font-cursive">
+                  Lorem ipsum dolor sit amet, consectetur dolorili adipiscing
+                  elit. Felis donec massa aliqua.
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <PinkSeparatorArrow />
+    </section>
+  );
+};
+
 const Perks = async () => {
   const perksData = await getPerksData();
 
