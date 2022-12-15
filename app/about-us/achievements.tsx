aec7de8a78eca317de78c910adcdcba5234e55d8
Ankita Trivedi <iamtrivediankita@gmail.com>
2022-12-15 18:03:33 +0530

    abous-us skeleton

--- app/about-us/achievements.tsx
+++ app/about-us/achievements.tsx
@@ -1,6 +1,30 @@
 import '@/styles/achieve.css';
 import React from 'react';
-
+export const AchievementsSkeleton = () => {
+  return (
+    <section className="achieve">
+      <h2 className="mx-0 mb-10 text-center text-6xl md:mb-8 lg:mx-72">
+        A few numbers that we are proud of
+      </h2>
+      <div className="achieve__content">
+        {[1, 2, 3].map((x) => (
+          <div className="overflow-hidden">
+            <h1 className="font-cursive text-secondary3">
+              {new Intl.NumberFormat('en-IN', {
+                maximumSignificantDigits: 3,
+              }).format(10000)}
+              +
+            </h1>
+            <h3 className="font-cursive">Students</h3>
+            <p className="font-cursive">
+              Lorem ipsum dolor sit amet, consectetur adipiscing
+            </p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 type Props = {};
 const achievedetails = [
   {
--- app/about-us/ourWorks.tsx
+++ app/about-us/ourWorks.tsx
@@ -2,7 +2,45 @@ import React from 'react';
 import RightSvg from '@/public/icons/right.svg';
 import '@/styles/ourWorks.css';
 import PinkSeparatorArrow from '@/ui/PinkSeparatorArrow';
-
+export const OurWorksSkeleton = () => {
+  return (
+    <section id="OurWorks" className="ourWorks">
+      <div className="ourWorks__header animate-pulse">
+        <h2 className="text-neutral-100">Our work values</h2>
+        <p className="ourWorks__line">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt.
+        </p>
+      </div>
+      <div className="ourWorks__content mx-auto lg:w-4/5">
+        {[1, 2, 3, 4].map((val) => (
+          <div className="ourWorks__sections">
+            <div className="ourWorks__chipLine">
+              <div className="ourWorks__svg rounded-full">
+                <div></div>
+              </div>
+              <div className="ourWorks__chip chip chip--white font-bold text-neutral-700">
+                <div></div>
+              </div>
+            </div>
+            <h3 className="ourWorks__h3 ">Company Name</h3>
+            <p className="ourWorks__p">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus
+              egestas non consequat pellentesque iaculis nunc, est, mollis.
+              Nulla.
+            </p>
+          </div>
+        ))}
+      </div>
+      <div>
+        {' '}
+        <div className="ourWorks__arrow">
+          <PinkSeparatorArrow />
+        </div>
+      </div>
+    </section>
+  );
+};
 type Props = {};
 const Data2 = [
   {
