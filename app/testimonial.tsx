345c051c0fc845ad8a782cde6dd4da011d593a88
dhara dave <imdavedhara@gmail.com>
2022-12-15 13:41:58 +0530

    homepage UI fixed

--- app/testimonial.tsx
+++ app/testimonial.tsx
@@ -80,7 +80,7 @@ const Testimonial = async () => {
             ?.attributes as UploadFile;
           return (
             <div
-              className="testimonial__card card"
+              className="testimonial__card card bg-neutral-100"
               style={{
                 maxWidth: 768,
               }}
