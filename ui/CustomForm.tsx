04f3e0e441328387183649915183b23f171f7ad0
PratikN7572 <subwaysurfersnewyork@gmail.com>
2022-12-20 19:37:47 +0530

    commit code

--- ui/CustomForm.tsx
+++ ui/CustomForm.tsx
@@ -1,33 +1,30 @@
 import clsx from 'clsx';
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field } from 'formik';
+
 import React from 'react';
-import FieldItem from './Field';
+
 
 type Props = {};
 
 const CustomForm = ({ buttonStyle, fields, ...rest }: Props) => {
   return (
-    <>
-      <Formik {...rest}>
-        {() => (
-          <Form className="form">
-            {fields.map((field) => {
-              return (
-                <FieldItem key={field.name} {...field} />
-              )
+    <Formik {...rest}>
+      {({ }) => (
+        <Form className="form ">
+          {fields.map((field) => {
+            return <Field key={field.name} {...field} />;
+          })}
+          <button
+            type="submit"
+            className={clsx('btn btn--primary', {
+              [buttonStyle]: !![buttonStyle],
             })}
-            <button
-              type="reset"
-              className={clsx('btn btn--primary', {
-                [buttonStyle]: !![buttonStyle],
-              })}
-            >
-              submit
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </>
+          >
+            submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
