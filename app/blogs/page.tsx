2720dfd6c3c77aef80e1e74fa7648e8f6cc26dde
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-21 18:16:03 +0530

    commit code

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import '@/styles/blogs.css';
 import BlogSubscribe, {
   BlogSubscribeSkeleton,
 } from 'app/training/blogSubscribe';
-import Icon from '@/ui/Icon';
 import Features, { FeaturesSkeleton } from 'app/courses/features';
-import PageHeader, { PageHeaderSkeleton } from '@/ui/pageHeader';
+import PageHeader from '@/ui/pageHeader';
 import Card from '@/ui/Card';
 import { Suspense } from 'react';
 import Blog from 'app/training/blog';
@@ -98,50 +95,7 @@ export default async function Page() {
       },
     },
   ];
-  const blogData = [
-    {
-      id: 1,
-      image_url:
-        'https://images.unsplash.com/photo-1667988152364-52ab908cd3bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
-      heading: 'Lorem ipsum dolor sit amet, consectetur adip',
-    },
-    {
-      id: 2,
-      image_url:
-        'https://images.unsplash.com/photo-1667988152364-52ab908cd3bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
 
-      heading: 'Lorem ipsum dolor sit amet, consectetur adip',
-    },
-    {
-      id: 3,
-      image_url:
-        'https://images.unsplash.com/photo-1667988152364-52ab908cd3bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60',
-
-      heading: 'Lorem ipsum dolor sit amet, consectetur adip',
-    },
-  ];
-  const data = {
-    isHorizontal: false,
-    // chip_align: 'left',
-    image_url:
-      'https://assets.website-files.com/607de2d8e8911ebf197a3f0f/607f2e01cbd8323965e6629a_image-6-courses-education-x-template.jpg',
-    heading: 'Graphic Design 101',
-    time: '8hr 12m',
-    rate: '99.00',
-    publishedAt: {
-      month: 'August',
-      day: '11',
-      year: '2022',
-    },
-    // description:
-    //   'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio, corrupti, eius quae accusantium alias eaque facere voluptatum aliquid fugit, harum sed quia quos dolorum nam!',
-    avatar_info: {
-      avatar_url:
-        'https://assets.website-files.com/607de2d8e8911ebf197a3f0f/607f1ff9fd9e0e1686d26497_image-3-profile-picture-small-teacher-education-x-template.jpg',
-      avatar_name: 'Yagnesh Modh',
-      avatar_Designation: 'Designer',
-    },
-  };
   return (
     <>
       <Suspense fallback={<h1>Loading...</h1>}>
@@ -154,6 +108,7 @@ export default async function Page() {
         />
       </Suspense>
       <Suspense fallback={<h1>heading...</h1>}>
+        {/* @ts-expect-error Async Server Component */}
         <Blog />
       </Suspense>
 
@@ -161,13 +116,30 @@ export default async function Page() {
         <BlogSubscribe />
       </Suspense>
       <Suspense fallback={<FeaturesSkeleton />}>
-        <Features />
+        <Features
+          title="All Courses"
+          chips={[
+            {
+              name: 'All',
+              selected: true,
+            },
+            {
+              name: 'Development',
+            },
+            {
+              name: 'Design',
+            },
+            {
+              name: 'Marketing',
+            },
+          ]}
+        />
       </Suspense>
-      <section className="blogs__moreblogs">
+      {/* <section className="blogs__moreblogs">
         {moreblogData.map((moreblogs: any) => {
           return <Card data={moreblogs} />;
         })}
-      </section>
+      </section> */}
     </>
   );
 }
