7a8205eb7418aa8d068564c5eb08b85af1d706c7
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2022-12-23 18:25:05 +0530

    about page & faq section chnages done

--- ui/SeparatorArray.tsx
+++ ui/SeparatorArray.tsx
@@ -8,7 +8,7 @@ const SeparatorArray = (props: Props) => {
   return (
     <div className="absolute -bottom-[84px] w-full">
       {/* <YellowSvg height={160} width={77} className="mx-auto" /> */}
-      {/* <PinkSvg height={160} width={77} className="mx-auto" /> */}
+      {/* <PinkSvg h-[120px] w-11 md:h-[173px] md:w-[65px] lg:h-[206px] lg:w-[77px]" /> */}
       <BlueSvg className="mx-auto h-[120px] w-11 md:h-[173px] md:w-[65px] lg:h-[206px] lg:w-[77px] " />
     </div>
   );
