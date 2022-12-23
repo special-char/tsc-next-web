a3e7448b1ba26fecfcf82744b4812dfedfa9ea63
dhara dave <imdavedhara@gmail.com>
2022-12-23 19:38:09 +0530

    eventsDetails

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const Page = () => {
   return (
-    <section className="events h-auto ">
+    <section className="events">
       <div>
         <div className="events__banner "></div>
         <div className="events__animate-bounce-slow1 "></div>
@@ -20,8 +20,9 @@ const Page = () => {
             fill
           />
         </div>
-        <EventDatailPage />
-
+        <div className="pt-8">
+          <EventDatailPage />
+        </div>
         <div className="events__paragraph">
           <h2 className="mb-0 text-3xl md:mb-2 md:text-6xl">Event Agenda</h2>
           <p>
@@ -58,6 +59,24 @@ const Page = () => {
         </div>
       </div>
     </section>
+    // <div className="wrapper">
+    //   <div className="wrapper__header">
+    //     <div className="events__animate-bounce-slow1 "></div>
+    //     <div className="events__animate-bounce-slow2"></div>
+    //     <div className="wrapper__body">
+    //       <div className="wrapper__body__image">
+    //         <Image
+    //           alt="It is the image of the blog post that you are viewing"
+    //           src={
+    //             'https://assets.website-files.com/607de2d8e8911ebf197a3f0f/607f23fe55a94aa127fb47b2_image-6-blog-education-x-template.jpg'
+    //           }
+    //           fill
+    //         />
+    //       </div>
+    //       <EventDatailPage />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
