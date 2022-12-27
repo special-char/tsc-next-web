51908b251b8b7a5051780faa89851647e64dea0c
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2022-12-27 17:48:14 +0530

    course datail accodian layout set

--- ui/Accordian.tsx
+++ ui/Accordian.tsx
@@ -16,8 +16,10 @@ const Accordian = ({ data }: Props) => {
   return (
     <>
       {data.map((val) => (
-        <details key={val.id} className="rounded-2xl md:py-8">
-          <summary>{val.title}</summary>
+        <details key={val.id} className="!mb-2 rounded-2xl !px-2 md:py-8">
+          <summary className="items-center !text-xl lg:!text-xxl">
+            {val.title}
+          </summary>
           <p
             dangerouslySetInnerHTML={{
               __html: md().render(val.description),
