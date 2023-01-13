246441fcf8a12b2c0eaf22bdd369811e4931a83b
Nirav Parekh <i.niravparekh@gmail.com>
2023-01-13 18:54:20 +0530

    corporate-training data added

---
+++ lib/getTrainingCourses.ts
@@ -0,0 +1,48 @@
+import { CourseEntityResponseCollection } from 'types/types';
+
+export type CoursesType = () => Promise<{
+  data: {
+    courses: CourseEntityResponseCollection;
+  };
+}>;
+
+export const getTrainingCourses: CoursesType = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
+      method: 'POST',
+      body: JSON.stringify({
+        query: `{
+          courses {
+            data {
+              attributes {
+                title
+                brochure {
+                  data {
+                    attributes {
+                      url
+                      alternativeText
+                    }
+                  }
+                }
+                curriculam {
+                  title
+                  content
+                }
+              }
+            }
+          }
+        }`,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      cache: 'no-cache',
+      next: {
+        revalidate: 0,
+      },
+    });
+
+    return await res.json();
+  } catch (error) {}
+};
--- ui/Accordian.tsx
+++ ui/Accordian.tsx
@@ -2,11 +2,14 @@
 
 import clsx from 'clsx';
 import md from 'markdown-it';
+import Link from 'next/link';
 
 export type AccordianType = {
   id: string;
   title: string;
   description: string;
+  curriculam?: { title: string; content: string }[];
+  brochure?: {};
 };
 
 type Props = {
@@ -17,21 +20,58 @@ type Props = {
 const Accordian = ({ data, hasTag }: Props) => {
   return (
     <>
-      {data.map((val) => (
-        <details
-          key={val.id}
-          className={clsx({
-            'with-tag': hasTag,
-          })}
-        >
-          <summary>{val.title}</summary>
-          <div
-            dangerouslySetInnerHTML={{
-              __html: md().render(val.description),
-            }}
-          ></div>
-        </details>
-      ))}
+      {data.map((val, index) => {
+        return (
+          <details
+            key={val.id}
+            className={clsx({
+              'with-tag': hasTag,
+            })}
+          >
+            <summary>{val.title}</summary>
+            {val?.curriculam?.map((item) => (
+              <>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: md().render(item.title),
+                  }}
+                ></div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: md().render(item.content),
+                  }}
+                ></div>
+              </>
+            ))}
+            {typeof val.description === 'string' &&
+              index === data.length - 1 && (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: md().render(val.description),
+                  }}
+                ></div>
+              )}
+          </details>
+        );
+      })}
+      {/* <div>
+        {descriptiondata.map((val) => {
+          return (
+            <>
+              <span>
+                {val.attributes?.curriculam?.map((val) => {
+                  return (
+                    <>
+                      <h6>{val?.title}</h6>
+                      <p>{val?.content}</p>
+                    </>
+                  );
+                })}
+              </span>
+            </>
+          );
+        })}
+      </div> */}
     </>
   );
 };
