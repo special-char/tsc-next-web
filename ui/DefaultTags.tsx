c21868f0a93de62d2bca0ff8e803a4566d2bac1d
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-05-03 15:59:14 +0530

    added facebook meta tag

--- ui/DefaultTags.tsx
+++ ui/DefaultTags.tsx
@@ -29,6 +29,10 @@ export function DefaultTags() {
       <meta name="application-name" content="TSC" />
       <meta name="msapplication-TileColor" content="#ffc40d" />
       <meta name="theme-color" content="#ffffff" />
+      <meta
+        name="facebook-domain-verification"
+        content="vigccmi1rqkvir4pf7xtlomkl3skuz"
+      />
       <link rel="icon" href="/icons/tsc.svg" type="image/svg+xml"></link>
     </>
   );
