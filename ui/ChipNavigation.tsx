243267c029407ca2be77ddf8b3360dd852db9d69
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2023-09-27 18:29:14 +0530

    course title aline

--- ui/ChipNavigation.tsx
+++ ui/ChipNavigation.tsx
@@ -6,7 +6,7 @@ import Button from './Button';
 
 export const ChipNavigationSkeleton = () => {
   return (
-    <div className="grid shrink-0 grid-cols-2 gap-4 rounded-3xl bg-neutral-200 px-8 py-4 md:grid-flow-col-dense md:rounded-full">
+    <div className="grid shrink-0 gap-4 rounded-3xl bg-neutral-200 px-8 py-4 md:grid-flow-col-dense md:grid-cols-2 md:rounded-full lg:grid-cols-none">
       {[1, 2, 3, 4].map((data) => (
         <div key={data} className="chip chip--white w-full font-cursive">
           Design
@@ -23,22 +23,26 @@ type ChipNavigationType = {
 
 const ChipNavigation = ({ chipData, selectedCategory, attributes }) => {
   return (
-    <div className="grid shrink-0 grid-cols-2 gap-4 rounded-3xl bg-neutral-200 px-8 py-4 md:grid-flow-col-dense md:rounded-full">
+    <div className="grid shrink-0 justify-items-center gap-y-2 rounded-3xl bg-neutral-200 py-4 md:grid-cols-2 md:rounded-full lg:flex">
       {chipData.map(({ ...data }, index) => {
-        if (!attributes || (attributes && attributes[`${data['key']}`].length > 0)) {
-          return <Button
-            key={index}
-            as="button"
-            className={clsx('chip w-full', {
-              'chip--primary': data.children === selectedCategory,
-              'chip--white': data.children !== selectedCategory,
-            })}
-            {...data}
-          />;
+        if (
+          !attributes ||
+          (attributes && attributes[`${data['key']}`].length > 0)
+        ) {
+          return (
+            <Button
+              key={index}
+              as="button"
+              className={clsx('chip w-full', {
+                'chip--primary': data.children === selectedCategory,
+                'chip--white': data.children !== selectedCategory,
+              })}
+              {...data}
+            />
+          );
         }
-        return null
-      }
-      )}
+        return null;
+      })}
     </div>
   );
 };
