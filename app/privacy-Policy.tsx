098a227e27bff86e20ce867d6f95265111e97275
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2023-10-11 19:50:12 +0530

    privacy page build

---
+++ app/privacy-Policy.tsx
@@ -0,0 +1,222 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  const sections = [
+    {
+      title: 'Data Controller and Owner',
+      content: (
+        <address>
+          <a href="https://www.google.com/maps/uv?pb=!1s0x395e83d506ef6f0f%3A0x550c5bf579c81861!3m1!7e115!4s%2Fmaps%2Fplace%2Fthe%2Bspecial%2Bcharacter%2F%4023.1144272%2C72.5402382%2C3a%2C75y%2C174.52h%2C90t%2Fdata%3D*213m4*211e1*213m2*211slF1r9c19nt_TOeKvQ8FKrQ*212e0*214m2*213m1*211s0x395e83d506ef6f0f%3A0x550c5bf579c81861%3Fsa%3DX%26ved%3D2ahUKEwi7i9aJk-6BAxWos1YBHcgGC9MQpx96BAhIEAA!5sthe%20special%20character%20-%20Google%20Search!15sCgIgAQ&imagekey=!1e2!2slF1r9c19nt_TOeKvQ8FKrQ&hl=en&sa=X&ved=2ahUKEwi7i9aJk-6BAxWos1YBHcgGC9MQpx96BAhjEA0">
+            Specialchar Pvt. Ltd.
+            <br />
+            604/605 6th Floor Ganesh Glory 11, Jagatpur Road, SG Highway,
+            Ahmedabad, Gujarat 382470
+          </a>
+          <br />
+          Email:
+          <Link href="mailto:contact@thespecialcharacter.com">
+            contact@thespecialcharacter.com
+          </Link>
+        </address>
+      ),
+    },
+    {
+      title: 'Types of Data collected',
+      content: (
+        <p>
+          Among the types of Personal Data that this Application collects, by
+          itself or through third parties, there are: Cookies and Usage Data.
+          Other Personal Data collected may be described in other sections of
+          this privacy policy or by dedicated explanation text contextually with
+          the Data collection. The Personal Data may be freely provided by the
+          User, or collected automatically when using this Application. Any use
+          of Cookies - or of other tracking tools - by this Application or by
+          the owners of third-party services used by this Application, unless
+          stated otherwise, serves to identify Users and remember their
+          preferences, for the sole purpose of providing the service required by
+          the User. Failure to provide certain Personal Data may make it
+          impossible for this Application to provide its services. Users are
+          responsible for any Personal Data of third parties obtained, published
+          or shared through this Application and confirm that they have the
+          third party's consent to provide the Data to the Owner. Mode and place
+          of processing the Data Methods of processing. The Data Controller
+          processes the Data of Users in a proper manner and shall take
+          appropriate security measures to prevent unauthorized access,
+          disclosure, modification, or unauthorized destruction of the Data. The
+          Data processing is carried out using computers and/or IT enabled
+          tools, following organizational procedures and modes strictly related
+          to the purposes indicated. In addition to the Data Controller, in some
+          cases, the Data may be accessible to certain types of persons in
+          charge, involved with the operation of the site (administration,
+          sales, marketing, legal, system administration) or external parties
+          (such as third-party technical service providers, mail carriers,
+          hosting providers, IT companies, communications agencies) appointed,
+          if necessary, as Data Processors by the Owner. The updated list of
+          these parties may be requested from the Data Controller at any time.
+          Place. The Data is processed at the Data Controller's operating
+          offices and in any other places where the parties involved with the
+          processing are located. For further information, please contact the
+          Data Controller.
+        </p>
+      ),
+    },
+    {
+      title: 'Retention time',
+      content: (
+        <p>
+          The Data is kept for the time necessary to provide the service
+          requested by the User, or stated by the purposes outlined in this
+          document, and the User can always request that the Data Controller
+          suspend or remove the data.
+        </p>
+      ),
+    },
+    {
+      title: 'The use of the collected Data',
+      content: (
+        <p>
+          The Data concerning the User is collected to allow the Owner to
+          provide its services, as well as for the following purposes: Access to
+          third party accounts and Analytics. The Personal Data used for each
+          purpose is outlined in the specific sections of this document.
+        </p>
+      ),
+    },
+    {
+      title: 'Facebook permissions asked by this Application',
+      content: (
+        <p>
+          This Application may ask for some Facebook permissions allowing it to
+          perform actions with the User's Facebook account and to retrieve
+          information, including Personal Data, from it. For more information
+          about the following permissions, refer to the Facebook permissions
+          documentation and to the Facebook privacy policy. The permissions
+          asked are the following
+        </p>
+      ),
+    },
+    {
+      title: 'Basic information',
+      content: (
+        <p>
+          By default, this includes certain Users€™s Data such as id, name,
+          picture, gender, and their locale. Certain connections of the User,
+          such as the Friends, are also available. If the User has made more of
+          their Data public, more information will be available. Detailed
+          information on the processing of Personal Data
+        </p>
+      ),
+    },
+    {
+      title:
+        'Personal Data is collected for the following purposes and using the following services:',
+      content: (
+        <p>
+          This type of service allows this Application to access Data from your
+          account on a third-party service and perform actions with it. These
+          services are not activated automatically, but require explicit
+          authorization by the User.
+        </p>
+      ),
+    },
+    {
+      title: 'Place of processing: US â€“ Privacy Policy',
+      content: (
+        <p>
+          The services contained in this section enable the Owner to monitor and
+          analyse web traffic and can be used to keep track of User behaviour.
+        </p>
+      ),
+    },
+    {
+      title: 'Google Analytics (Google Inc.)',
+      content: (
+        <p>
+          Google Analytics is a web analysis service provided by Google Inc.
+          (â€œGoogleâ€). Google utilizes the Data collected to track and examine
+          the use of this Application, to prepare reports on its activities and
+          share them with other Google services. Google may use the Data
+          collected to contextualize and personalize the ads of its own
+          advertising network. Personal Data collected: Cookies and Usage Data.
+          Place of processing: US â€“ Privacy Policy â€“ opt Out
+          <br />
+          Additional information about Data collection and processing Legal
+          action
+          <br />
+          The User's Personal Data may be used for legal purposes by the Data
+          Controller, in Court or in the stages leading to possible legal action
+          arising from improper use of this Application or the related services.
+          The User declares to be aware that the Data Controller may be required
+          to reveal personal data upon request of public authorities.
+          <br />
+          Additional information about User's Personal Data
+          <br />
+          In addition to the information contained in this privacy policy, this
+          Application may provide the User with additional and contextual
+          information concerning particular services or the collection and
+          processing of Personal Data upon request. System logs and maintenance
+          <br />
+          For operation and maintenance purposes, this Application and any
+          third-party services may collect files that record interaction with
+          this Application (System logs) or use for this purpose other Personal
+          Data (such as IP Address).
+        </p>
+      ),
+    },
+    {
+      title: 'Information not contained in this policy',
+      content: (
+        <p>
+          More details concerning the collection or processing of Personal Data
+          may be requested from the Data Controller at any time. Please see the
+          contact information at the beginning of this document. The rights of
+          Users
+          <br />
+          Users have the right, at any time, to know whether their Personal Data
+          has been stored and can consult the Data Controller to learn about
+          their contents and origin, to verify their accuracy or to ask for them
+          to be supplemented, cancelled, updated or corrected, or for their
+          transformation into anonymous format or to block any data held in
+          violation of the law, as well as to oppose their treatment for any and
+          all legitimate reasons. Requests should be sent to the Data Controller
+          at the contact information set out above. This Application does not
+          support â€œDo Not Trackâ€ requests.
+          <br />
+          To determine whether any of the third-party services it uses honor the
+          â€œDo Not Trackâ€ requests, please read their privacy policies.
+        </p>
+      ),
+    },
+    {
+      title: 'Changes to this privacy policy',
+      content: (
+        <p>
+          The Data Controller reserves the right to make changes to this privacy
+          policy at any time by giving notice to its Users on this page. It is
+          strongly recommended to check this page often, referring to the date
+          of the last modification listed at the bottom. If a User objects to
+          any of the changes to the Policy, the User must cease using this
+          Application and can request that the Data Controller remove the
+          Personal Data. Unless stated otherwise, the then-current privacy
+          policy applies to all Personal Data the Data Controller has about
+          Users.
+        </p>
+      ),
+    },
+  ];
+
+  return (
+    <section className="px-container py-20" id="PrivacyPolicy">
+      <h1>Privacy Policy - Specialchar Pvt. Ltd.</h1>
+      {sections.map((section, index) => (
+        <div key={index}>
+          <h4 className="pt-4">{section.title}</h4>
+          {section.content}
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default PrivacyPolicy;
