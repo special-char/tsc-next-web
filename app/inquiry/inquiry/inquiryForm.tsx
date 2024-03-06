e6f793a083bd39643e8b2e0d99f03151109a1c5d
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2024-03-06 15:37:55 +0530

    inquiryPage build

---
+++ app/inquiry/inquiry/inquiryForm.tsx
@@ -0,0 +1,23 @@
+
+import { getFormDetails } from '@/lib/getFormDetails';
+import DynamicForm from '@/ui/DynamicForm';
+import { use } from 'react';
+
+
+type Props = {};
+
+const InquiryForm = async (props: Props) => {
+  const formDetails = await getFormDetails(4);
+
+  const fields = formDetails.data?.attributes?.fields;
+
+  const submitUrl = formDetails.data?.attributes?.submitURL as string;
+
+  return (
+    <div className="flex flex-col px-container py-4">
+      <DynamicForm buttonStyle="px-3 py-3" fields={fields} submitUrl={submitUrl} />
+    </div>
+  );
+};
+
+export default InquiryForm;
