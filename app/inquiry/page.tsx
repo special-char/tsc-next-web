22981c91acd8b560dd93c5411a2b4989cce1fc84
Pruthvish Modi <Iampruthvishmodi@gmail.com>
2024-03-06 09:43:36 +0530

    inquiry form page build

---
+++ app/inquiry/page.tsx
@@ -0,0 +1,16 @@
+// components/InquiryForm.js
+
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import InquiryForm from './inquiryForm';
+
+
+const Inquiry = () => {
+  return (
+    
+   <InquiryForm/>
+  );
+};
+
+export default Inquiry;
+
